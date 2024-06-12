471168ece170a98698c2003591793207d1102ac6
ansetechnoapp <129697695+ansetechnoapp@users.noreply.github.com>
2024-06-12 11:31:23 +0100

    edit astro config 6

--- src/constants/link-presets.ts
+++ src/constants/link-presets.ts
@@ -5,7 +5,7 @@ import { i18n } from '@i18n/translation'
 export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
   [LinkPreset.Home]: {
     name: i18n(I18nKey.home),
-    url: '/',
+    url: '/blocAstroJs2/',
   },
   [LinkPreset.About]: {
     name: i18n(I18nKey.about),
